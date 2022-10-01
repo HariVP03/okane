22e909243bf53ad12e65806f98d27a0a56292718
Hari Vishnu Parashar <harryskotch11@gmail.com>
2022-10-01 21:32:59 +0530

    fix

--- src/helpers/upi-string.ts
+++ src/helpers/upi-string.ts
@@ -1,7 +1,10 @@
 import { Platform } from "react-native";
 
 export function parseUpiString(upi: string, amount: number) {
-  const upiWithAmountFixed = `${upi.replace("&am=", "")}&am=100`;
+  const upiWithAmountFixed = `${upi.replace(
+    "&am=",
+    ""
+  )}&am=${amount.toString()}`;
 
   if (Platform.OS === "ios") {
     return upiWithAmountFixed.replace("upi://", "tez://upi/");
